b950dc536c3dfd586bcc7d510da545c68a503646
cruzguzmanes-dev <cruzguzmanes.dev@gmail.com>
2026-07-30 01:41:10 -0600

    update error

--- api/distribuidor/test.js
+++
@@ -1,5 +0,0 @@
-export default async function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Content-Type', 'application/json');
-  return res.status(200).json({ message: 'Test endpoint works!', timestamp: new Date().toISOString() });
-}
